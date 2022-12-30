3786e56dd1ff08c6585801f9db24af2914894c9e
Aleyna Elif ÖZKAN <51717390+aleynaelif@users.noreply.github.com>
2022-12-31 02:51:41 +0300

    api

--- app/src/main/java/com/ley/insp/SurveyFragment.kt
+++ app/src/main/java/com/ley/insp/SurveyFragment.kt
@@ -1,6 +1,8 @@
 package com.ley.insp
 
+import android.net.Uri.encode
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,11 +12,20 @@ import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
 import com.ley.insp.databinding.FragmentSurveyBinding
+import okhttp3.*
+import java.io.IOException
+import java.net.URLDecoder
+import java.net.URLEncoder
+import java.net.URLEncoder.encode
+import java.util.concurrent.TimeUnit
+
 
 class SurveyFragment : Fragment() {
 
     private var _binding: FragmentSurveyBinding? = null
     private val binding get() = _binding!!
+    val client = OkHttpClient()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,7 +59,31 @@ class SurveyFragment : Fragment() {
         if (result.contents == null) {
             Toast.makeText(activity, "Cancelled", Toast.LENGTH_LONG).show()
         } else {
-            Toast.makeText(activity, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+
+//nutella 59032823
+
+            val request = Request.Builder()
+                .url("https://nutritionix-api.p.rapidapi.com/v1_1/item?upc="+ result.contents)
+                .get()
+                .addHeader("X-RapidAPI-Key", "8277f3b565mshf7a96c63bddb053p113ab0jsn8ba9ba3d7ce8")
+                .addHeader("X-RapidAPI-Host", "nutritionix-api.p.rapidapi.com")
+                .build()
+
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException)  {e.printStackTrace()}
+                override fun onResponse(call: Call, response: Response) {
+
+                    var product = response.body()?.string()!!.toString()
+
+                    Log.d("TAGDENEME",product)
+
+                    //teker teker tarama
+                    println("Server: ${response.header("allergen_contains_fish")}")
+
+                }
+            })
+          //  Toast.makeText(activity, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
         }
     }
 
